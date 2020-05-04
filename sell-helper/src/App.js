54a7e7efcb0be1bcc8ffe3229b82640db0cfcbe6
Xingkai Yang <xingkaiyang@Xingkais-MacBook-Pro.local>
2020-05-03 22:37:25 -0400

    extract data to app.jsx

--- sell-helper/src/App.js
+++ sell-helper/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
@@ -7,19 +7,62 @@ import CharactersPage from "./components/charactersPage";
 import ResultPage from "./components/resultPage";
 import GamePage from "./components/gamePage";
 
-function App() {
-  return (
-    <React.Fragment>
-      <WelcomePic />
-      <Switch>
-        <Route path="/charactersPage/:gameName?" component={CharactersPage} />
-        <Route path="/charactersPage" component={CharactersPage} />
+class App extends Component {
+  state = {
+    games: [
+      {
+        gameName: "公主链接",
+        gamePicURL: require("./components/pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
+        servers: ["苹果", "安卓"],
+        characters: [
+          {
+            name: "公主链接",
+            picURL: require("./components/pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
+            lvl: 0,
+          },
+          {
+            name: "公主链接2",
+            picURL: require("./components/pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
+            lvl: 0,
+          },
+          {
+            name: "公主链接3ß",
+            picURL: require("./components/pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
+            lvl: 0,
+          },
+        ],
+      },
+      {
+        gameName: "山海镜花",
+        gamePicURL: require("./components/pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
+        servers: ["苹果", "安卓"],
+        characters: [],
+      },
+    ],
+  };
 
-        <Route path="/resultPage" component={ResultPage} />
-        <Route path="/" component={GamePage} />
-      </Switch>
-    </React.Fragment>
-  );
+  render() {
+    return (
+      <React.Fragment>
+        <WelcomePic />
+        <Switch>
+          <Route
+            path="/charactersPage/:gameName"
+            render={(props) => (
+              <CharactersPage games={this.state.games} {...props} />
+            )}
+          />
+          <Route path="/charactersPage/" component={CharactersPage} />
+
+          <Route path="/resultPage" component={ResultPage} />
+          <Route
+            path="/"
+            render={(props) => <GamePage games={this.state.games} {...props} />}
+          />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- sell-helper/src/components/gamePage.jsx
+++ sell-helper/src/components/gamePage.jsx
@@ -5,43 +5,14 @@ import Game from "./common/game";
 
 class GamePage extends Component {
   state = {
-    games: [
-      {
-        gameName: "公主链接",
-        gamePicURL: require("./pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
-        servers: ["苹果", "安卓"],
-        characters: [
-          {
-            name: "公主链接",
-            picURL: require("./pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
-            lvl: 0,
-          },
-          {
-            name: "公主链接2",
-            picURL: require("./pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
-            lvl: 0,
-          },
-          {
-            name: "公主链接3ß",
-            picURL: require("./pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
-            lvl: 0,
-          },
-        ],
-      },
-      {
-        gameName: "山海镜花",
-        gamePicURL: require("./pic/characterPic/fe52a0c185314b9781eb030800b15156.png"),
-        servers: ["苹果", "安卓"],
-        characters: [],
-      },
-    ],
+    games: [],
   };
 
   render() {
     return (
       <div>
         <SearchBar></SearchBar>
-        {this.state.games.map((game) => (
+        {this.props.games.map((game) => (
           <Link key={game.gameName} to={`/charactersPage/${game.gameName}`}>
             <Game gameName={game.gameName} gamePicURL={game.gamePicURL} />
           </Link>
